75c635b417426e8f03d7885cf960d54b2557e6dd
Pavel Selivanov <selivan.at.github@gmail.com>
2017-12-26 06:04:29 +0300

    vagrant: multiple machines: work in progress

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+build-kernel() {
+    set -x
+    vmlinuz_path=$(readlink -f /vmlinuz)
+    vmlinuz_filename=$(basename $vmlinuz_path)
+    cp "$vmlinuz_path" /vagrant/build
+    cd /vagrant/build
+    ln -sf "vmlinuz_filename" vnlinuz
+    cd -
+}
+
+build-initrd() {
+    set -x
+    update-initramfs -u
+    initrd_path=$(readlink -f /initrd.img)
+    initrd_filename=$(basename $initrd_path)
+    cp "$initrd_path" /vagrant/build
+    cd /vagrant/build/
+    ln -sf "$initrd_filename" initrd.img
+    cd -
+}
+
+build-rootfs() {
+    set -x
+    mv /vagrant/build/rootfs.squashfs /vagrant/build/rootfs.squashfs.bak
+    cd /
+    # -always-use-fragments use fragment blocks for files larger than block size
+    # -noappend do not append to existing filesystem
+    #
+    mksquashfs . /vagrant/build/rootfs.squashfs -noappend -always-use-fragments -comp xz -wildcards -ef $(dirname $0)/rootfs.exclude_dirs
+    cd -
+    rm -f /vagrant/build/rootfs.squashfs.bak
+}
+
+build-home() {
+    set -x
+    mv /vagrant/build/home.tar.gz /vagrant/build/home.tar.gz.bak
+    cd /home/ubuntu
+    # Don't use default unsecure ssh keys
+    tar --exclude="./ubuntu/.ssh" -czf /vagrant/build/home.tar.gz ./
+    cd -
+    rm -f /vagrant/build/home.tar.gz.bak
+}
+
+case "$1" in
+    all)
+        build-kernel
+        build-initrd
+        build-rootfs
+        build-home
+        ;;
+    kernel)
+        build-kernel
+        ;;
+    initrd)
+        build-initrd
+        ;;
+    rootfs)
+        build-rootfs
+        ;;
+    home)
+        build-home
+        ;;
+    *)
+        echo "Usage: $0 all|initrd|kernel|rootfs|home"
+        exit 1
+        ;;
+esac
--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#set -x
-[ -z "$1" ] && echo "Usage: $0 VERSION" && exit 1
-cd `dirname $0`
-mkdir -p "./release/$1" || ( echo "Failed to create specified catalog" > /dev/stderr; exit 2 )
-cp -rv ./build/* "./release/$1"
-touch "./release/$1/version_$1"
-
