d7b9c38b3d573823f85f4bb95033b7aecc2622d8
Pavel Selivanov <selivan.at.github@gmail.com>
2017-12-31 23:09:43 +0300

    fixed extract-* scripts

--- extract-initrd.sh
+++ extract-initrd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
 
 cd $(dirname $0)
-mkdir -p unpacked-initrd
+mkdir -p ./unpacked-initrd
 # Clean old files
 find ./unpacked-initrd/ -mindepth 1 -depth -delete
 cd unpacked-initrd
-bzip2 -dc ../initrd.img | cpio -id
+initrd=$(readlink -f ../build/initrd.img)
+bzip2 -dc "$initrd" | cpio -id
 cd -
--- extract-rootfs.sh
+++ extract-rootfs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 
 cd $(dirname $0)
-mkdir -p unpacked-rootfs
 # Clean old files
 find ./unpacked-rootfs/ -mindepth 1 -depth -delete
-unsquashfs -d unpacked-rootfs build/$image_name.squashfs
+rmdir ./unpacked-rootfs/
+unsquashfs -d unpacked-rootfs build/rootfs.squashfs
 cd -
